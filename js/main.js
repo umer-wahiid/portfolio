580b16e231c4a83d3d4bb2766a7fe35b6d338c0c
ansari7016 <ammaransari7016@gmail.com>
2022-09-05 16:17:19 +0500

    nav bar changes

--- js/main.js
+++ js/main.js
@@ -289,3 +289,10 @@
 	});		
 
 })(jQuery);
+/* Header Scroll
+   ------------------------------------------------------- */
+
+   window.addEventListener("scroll",function(){
+	var header = document.querySelector("header");
+	header.classList.toggle("sticky",window.scrollY > 0);
+   })
